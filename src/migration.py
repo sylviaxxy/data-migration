b1d3e2f3c1f04a711d26d689ee5b6b4b6d65a9bd
sylviaxxy <xuxinyusylvia@outlook.com>
2019-02-23 22:49:34 -0500

    Add run.sh

--- src/migration.py
+++ src/migration.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import sys
 import os
 import psycopg2
 import json
@@ -5,16 +7,15 @@ import csv
 import datetime
 import pandas as pd
 
+#def get_column_names(path='../data/2017-10-30.json'):
+    #with open(path, 'rb') as f:
+        #record_list = json.load(f)
+        #col_name_list = []
+        #for col_name in record_list['orders'][0].keys():
+            #col_name_list.append(col_name)
+    #return col_name_list
 
-def get_column_names(path='../data/2017-10-30.json'):
-    with open(path, 'rb') as f:
-        record_list = json.load(f)
-        col_name_list = []
-        for col_name in record_list['orders'][0].keys():
-            col_name_list.append(col_name)
-    return col_name_list
-
-col_name_list = get_column_names('../data/2017-10-30.json')
+#col_name_list = get_column_names('../data/2017-10-30.json')
 
 def define_col_list():
     order_col_list = ["source_identifier",
@@ -70,22 +71,20 @@ def define_col_list():
                      "phone",
                      "name"]
 
-    line_item_list = ["quantity",
+    line_item_col_list = ["quantity",
                       "product_id",
                       "id",
                       "variant_id",
                       "order_id"]
-    return order_col_list, user_col_list, line_item_list
-
-order_col_list, user_col_list, line_item_list = define_col_list()
-
-
-json_file_path_1 = '../data/2017-10-30.json'
+    return order_col_list, user_col_list, line_item_col_list
 
 def transform_file(json_file_path):
+
+    order_col_list, user_col_list, line_item_col_list = define_col_list()
     order_df = pd.DataFrame(columns=order_col_list)
     user_df = pd.DataFrame(columns=user_col_list)
-    line_item_df = pd.DataFrame(columns=line_item_list)
+    line_item_df = pd.DataFrame(columns=line_item_col_list)
+
     with open(json_file_path, 'rb') as f:
         order_dict = json.load(f)
         for line in order_dict['orders']:
@@ -101,47 +100,84 @@ def transform_file(json_file_path):
 
             for line_item_line in line['line_items']:
                 line_item_new_row = {}
-                for line_item_col in line_item_list[:-1]:
+                for line_item_col in line_item_col_list[:-1]:
                     line_item_new_row[line_item_col] = line_item_line[line_item_col]
                 line_item_new_row['order_id'] = line['id']
                 line_item_df = line_item_df.append(line_item_new_row, ignore_index=True)
     return order_df, user_df, line_item_df
 
-order_df, user_df, line_item_df = transform_file(json_file_path_1)
 
-col_num = len(order_df.iloc[0,:])
-s = "%s,"*(col_num-1) + "%s"
-sql_query_templates = "INSERT INTO Orders ({0}) VALUES ({1})".format(','.join(order_col_list), s)
-sql_query = sql_query_templates, tuple(order_df.values.tolist()[0])
+def write_order_df(order_df,order_col_list):
 
-conn = psycopg2.connect(host="localhost", database="business", user="insight", password="qwertyui123")
-print("Database Connected")
-cur = conn.cursor()
-cur.execute(sql_query)
-conn.commit()
-conn.close()
+    conn = psycopg2.connect(host="localhost",
+                            database="business",
+                            user="insight",
+                            password="qwertyui123")
+    print("Database Connected")
+    cur = conn.cursor()
+    for i in range(len(order_df)):
+        col_num = len(order_df.iloc[0, :])
+        s = "%s," * (col_num - 1) + "%s"
+        sql_query = """INSERT INTO Orders ({0}) VALUES ({1})""".format(','.join(order_col_list), s)
+        record_to_insert = tuple(order_df.values.tolist()[i])
+        cur.execute(sql_query, record_to_insert)
+        conn.commit()
+    cur.close()
+    conn.close()
+    print ("Successfully wrote the order_df into Postgresql database.")
 
-#def write_file(order_df,user_df,line_item_df):
-    #conn = psycopg2.connect(host="localhost", database="Business", user="insight", password="qwertyui123")
-    #print("Database Connected")
-    #cur = conn.cursor()
-    #for i in len(order_df):
-        #cur.execute("INSERT INTO Orders {col} VALUES {val}".format(col = ','.join(order_col_list, val = (order_df.iloc[i,:]))
+def write_user_df(user_df,user_col_list):
 
-    #conn.commit()
-    #cur.close()
-    #conn.close()
-    #print ("Successfully write the df into Postgresql database.")
+    conn = psycopg2.connect(host="localhost",
+                            database="business",
+                            user="insight",
+                            password="qwertyui123")
+    print("Database Connected")
+    cur = conn.cursor()
+    for i in range(len(user_df)):
+        col_num = len(user_df.iloc[i, :])
+        s = "%s," * (col_num - 1) + "%s"
+        sql_query = """INSERT INTO Users ({0}) VALUES ({1})""".format(','.join(user_col_list), s)
+        record_to_insert = tuple(user_df.values.tolist()[i])
+        cur.execute(sql_query, record_to_insert)
+        conn.commit()
+    cur.close()
+    conn.close()
+    print ("Successfully wrote the user_df into Postgresql database.")
 
-path = '../data'
+def write_line_item_df(line_item_df,line_item_col_list):
+    conn = psycopg2.connect(host="localhost",
+                            database="business",
+                            user="insight",
+                            password="qwertyui123")
+    print("Database Connected")
+    cur = conn.cursor()
+    for i in range(len(line_item_df)):
+        col_num = len(line_item_df.iloc[i, :])
+        s = "%s," * (col_num - 1) + "%s"
+        sql_query = """INSERT INTO Line_items ({0}) VALUES ({1})""".format(','.join(line_item_col_list), s)
+        record_to_insert = tuple(line_item_df.values.tolist()[i])
+        cur.execute(sql_query, record_to_insert)
+        conn.commit()
+    cur.close()
+    conn.close()
+    print ("Successfully wrote the line_item_df into Postgresql database.")
 
 def dump_directory(path):
+    order_col_list, user_col_list, line_item_col_list = define_col_list()
     for file in os.listdir(path):
         if not file.startswith('.'):
             json_file_path = os.path.join(path, file)
             print("Now start to process all json files under data folder .", str(file))
             order_df, user_df, line_item_df = transform_file(json_file_path)
-            write_file(order_df, user_df, line_item_df)
+            write_order_df(order_df, order_col_list)
+            write_user_df(user_df, user_col_list)
+            write_line_item_df(line_item_df,line_item_col_list)
+            print("Processing job of", str(file),"was completed.")
     print("All json files under the data folder has been migrated into Postgresql database.")
 
-#dump_directory(path)
+def main(path):
+    dump_directory(path)
+
+if __name__ == "__main__":
+   main(sys.argv[1])
---
+++ src/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Use this shell script to compile (if necessary) your code and then execute it.
+python ./src/migration.py ./data
